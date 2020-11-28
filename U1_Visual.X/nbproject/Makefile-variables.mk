328c370976f37f59a72156264cbb54dda14c708e
anthonyibrail <75114333+anthonyibrail@users.noreply.github.com>
2020-11-27 19:48:55 -0800

    PIC U1 encargado de la parte visual

---
+++ U1_Visual.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=U1_Visual.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/U1_Visual.X.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=u1visual.x.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/u1visual.x.tar
